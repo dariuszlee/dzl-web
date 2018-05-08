dbbaa4e8ff0cb16f66351d1b9ac72c0ef8e443ed
dariuszlee <34044340+dariuszlee@users.noreply.github.com>
2018-05-07 21:48:58 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import(
+    "fmt"
+    "net/http"
+    "log"
+)
+
+func handler(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+
+func main(){
+    http.HandleFunc("/", handler)
+    log.Info("hello")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
