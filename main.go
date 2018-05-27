986cc4316535bf7be0d20c3ea1998b914d4b8cd7
dzlyy <dariuszlee93@gmail.com>
2018-05-27 19:51:44 -0400

    Simplified Layout

--- main.go
+++ main.go
@@ -6,16 +6,28 @@ import(
     "log"
 )
 
+type blog struct{
+	Title string
+}
+
 func handler(w http.ResponseWriter, r *http.Request){
 	tmpl, _ := template.ParseFiles("./html/layout.html", "./html/home.html")
 	tmpl.ExecuteTemplate(w, "layout", nil)
 }
 
+func handlerBlog(w http.ResponseWriter, r *http.Request){
+	b := blog{}
+	b.Title = r.URL.Path[len("/blog/"):]
+	log.Printf("title: %s", b.Title)
+	tmpl, _ := template.ParseFiles("./html/layout.html", "./html/blog.html")
+	tmpl.ExecuteTemplate(w, "layout", b)
+}
 
 func main(){
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
+	http.HandleFunc("/blog/", handlerBlog)
     http.HandleFunc("/", handler)
     log.Println("Listening...")
     http.ListenAndServe(":8080", nil)
