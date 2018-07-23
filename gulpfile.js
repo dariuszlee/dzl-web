55c94c339d6a7afcf155ac3abdc15447b5a8d9d7
dariuszlee <dariuszlee93@gmail.com>
2018-07-23 08:01:29 -0400

    Adding gulp definitions

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+
+// TODO: Markdown Generation
+// TODO: Image Compression
+// TODO: CSS minification (other minification where necessary)
+gulp.task('default', function() {
+
+});
